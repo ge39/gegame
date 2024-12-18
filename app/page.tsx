7bce069f1bbc9941a6332c18b46cf5fe6d1f7934
ge39 <ge39.medeiros@gmail.com>
2024-12-17 23:00:53 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -15,9 +15,14 @@ export default function Home() {
       <section
         id="home"
         style={{
-          backgroundColor: "#F5DEB3",
-          padding: "50px 20px",
+          backgroundImage: "url('/images/Fliperamas/arcade3.jpg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          width: "100%",
+          height: "700px",
           position: "relative",
+          
         }}
       >
         <h1
@@ -29,40 +34,37 @@ export default function Home() {
             color: "snow",
             fontSize: "24px",
             fontWeight: "bold",
-            marginTop: "-40px",
+            // marginTop: "-40px",
             marginBottom: "10px",
+            width:'100%',
           }}
         >
           Fliperamas Retrô, Nostalgia para Seus Eventos
         </h1>
           <div style={{ display: "flex", justifyContent: "center", marginBottom: "20px" }}>
-            <Image
-              src="/images/Fliperamas/arcade3.jpg"
-              alt="Fliperama"
-              width={1000}
-              height={200}
-              style={{ borderRadius: "8px", objectFit: "cover" }}
-            />
+                                                              
           </div>
         <p
           style={{
             position: "absolute",
-            top: "55%",
+            top: "45%",
             left: "50%",
             transform: "translate(-50%, -50%)",
-            backgroundColor: "rgba(230, 230, 250, 0.10)",
+            backgroundColor: "rgba(230, 230, 250, 0.90)",
             borderRadius: "10px",
             width: "85%",
-            color: "snow",
-            fontSize: "90%",
+            color: "#333",
+            fontSize: "100%",
             fontWeight: "bold",
             textAlign: "center",
-            maxWidth: "800px",
+            maxWidth: "900px",
+            padding:'10px',
+            
           }}
         >
           ALUGAMOS FLIPERAMAS COM JOGOS RETRÔ PARA TODOS OS TIPOS DE EVENTOS...
         </p>
-        <div style={{ textAlign: "center" }}>
+        <div style={{ textAlign: "center", marginTop:'400px', }}>
           <a
             href="#top"
             style={{
@@ -91,6 +93,7 @@ export default function Home() {
            color: "#204080",
            fontWeight: "bold",
            marginBottom: "20px",
+           fontSize:'24px',
          }}
        >
          NOSSOS PLANOS
@@ -99,7 +102,7 @@ export default function Home() {
          <Image
            src="/images/eventos/Fliperama_locacao.jpg"
            alt="Fliperama"
-           width={1000}
+           width={1600}
            height={200}
            style={{ borderRadius: "8px", objectFit: "cover" }}
          />
@@ -122,45 +125,44 @@ export default function Home() {
       <section
         id="orcamento"
         style={{
-          backgroundColor: "#77c5d5",
+          backgroundImage: "url('/images/Fliperamas/arcade2.jpg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          width: "100%",
+          height: "600px",
           padding: "50px 20px",
           position: "relative",
         }}
       >
         <h1
           style={{
+            padding:'20px',
             textAlign: "center",
-            color: "#204080",
+            color: "snow",
             fontWeight: "bold",
-            marginBottom: "20px",
+            // marginTop: "10%",
           }}
         >
           FALE CONOSCO
         </h1>
         <div style={{
            display: "flex", justifyContent: "center", marginBottom: "20px"  }}>
-          <Image
-            src="/images/Fliperamas/arcade2.jpg"
-            alt="Fliperama"
-            width={1000}
-            height={200}
-            style={{ borderRadius: "8px", objectFit: "cover" }}
-          />
         </div>
         <p
           style={{
             position: "absolute",
-            top: "55%",
+            top: "35%",
             left: "50%",
             transform: "translate(-50%, -50%)",
-            backgroundColor: "rgba(230, 230, 250, 0.10)",
+            backgroundColor: "rgba(230, 230, 250, 0.90)",
             borderRadius: "10px",
-            width: "85%",
-            color: "snow",
+            width: "90%",
+            color: "#333",
             fontSize: "90%",
             fontWeight: "bold",
             textAlign: "center",
-            maxWidth: "800px",   
+            maxWidth: "900px",   
           }}
         >
           ENTRE EM CONTATO PARA RECEBER UM ORÇAMENTO PERSONALIZADO...
@@ -172,10 +174,12 @@ export default function Home() {
             href="https://wa.me/5511975145360"
             target="_blank"
             style={{
+              marginTop:'250px',
               backgroundColor: "#25d366",
               padding: "10px 20px",
               borderRadius: "5px",
               color: "#fff",
+              fontSize: "100%",
               display: "inline-block",
               marginBottom:"18px",
             }}
@@ -183,10 +187,11 @@ export default function Home() {
             Fale pelo WhatsApp
           </a>
         </div>
-        <div style={{ textAlign: "center" }}>
+        <div style={{ textAlign: "center"}}>
             <a
               href="#top"
               style={{
+                left:"50%",
                 backgroundColor: "#FF4500",
                 padding: "10px 20px",
                 borderRadius: "5px",
@@ -202,16 +207,22 @@ export default function Home() {
       <section
         id="eventos"
         style={{
-          backgroundColor: "#F5DEB3",
-          padding: "50px 20px",
+          backgroundImage: "url('/images/fliperamas/arcade2.jpg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          width: "100%",
+          
+          height: "100%",
           position: "relative",
+        
         }}
       >
         <h1
           style={{
-            backgroundColor: "lightgrey",
+            // backgroundColor: "lightgrey",
             textAlign: "center",
-            color: "#204080",
+            color: "snow",
             fontWeight: "bold",
             marginBottom: "20px",
             padding:"10px",
@@ -222,7 +233,7 @@ export default function Home() {
 
         {/* Adicionando o Componente EventSection */}
         <EventSection />
-        <div style={{ textAlign: "center" }}>
+        <div style={{ textAlign: "center",padding: "40px 20px", }}>
            <a
              href="#top"
              style={{
@@ -241,15 +252,22 @@ export default function Home() {
       <section
         id="sobre"
         style={{
-          backgroundColor: "#77c5d5",
-          padding: "50px 20px",
+          // backgroundColor: "#77c5d5",
+          backgroundImage: "url('/images/Fliperamas/arcade3.jpg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          width: "100%",
+          height: "600px",
           position: "relative",
+
         }}
       >
         <h1
           style={{
+            padding:'60px',
             textAlign: "center",
-            color: "#204080",
+            color: "snow",
             fontWeight: "bold",
             marginBottom: "20px",
           }}
@@ -257,49 +275,43 @@ export default function Home() {
           SOBRE NÓS
           <p>Conheça nossa história e paixão por fliperamas retrô...</p>
         </h1>
-        <div style={{ textAlign: "center", marginBottom: "20px" }}>
-          <Image
-            src="/images/assets/arts/Eidos.jpg"
-            alt="Fliperama"
-            width={1000}
-            height={200}
-            style={{ borderRadius: "8px", objectFit: "cover" }}
-          />
-        </div>
         <p
           style={{
             position: "absolute",
-            top: "55%",
+            top: "250px",
             left: "50%",
             transform: "translate(-50%, -50%)",
             backgroundColor: "rgba(230, 230, 250, 0.7)",
             borderRadius: "10px",
-            width: "85%",
-            color: "black",
-            fontSize: "60%",
+            padding:'10px',
+            width: "90%",
+            color: "#333",
+            fontSize: "90%",
             fontWeight: "bold",
-            textAlign: "justify",
-            padding:"5px",
-            maxWidth: "800px",
+            // textAlign: "justify",
+            maxWidth: "1000px",
           
           }}
         >
          Somos apaixonados por jogos retrô e diversão de alta qualidade! Nosso objetivo é transformar eventos em experiências inesquecíveis, oferecendo o aluguel de fliperamas clássicos que trazem toda a magia e nostalgia dos jogos arcade. Com uma seleção cuidadosa de Jogos, recriamos o ambiente vibrante dos fliperamas dos anos 80 e 90, garantindo momentos únicos para festas, eventos corporativos, casamentos e celebrações em geral. Acreditamos no poder dos jogos de unir pessoas e criar memórias especiais. Por isso, nos dedicamos a entregar um serviço completo, com equipamentos de alta qualidade, suporte técnico e personalização para atender às suas necessidades. Reviva os clássicos, encante seus convidados e leve a diversão a outro nível com nossos fliperamas retrô!
         </p>
-        <div style={{ textAlign: "center" }}>
+        
+        <div style={{ textAlign: "center",marginTop:'300px', }}>
            <a
              href="#top"
              style={{
-               backgroundColor: "#FF4500",
-               padding: "10px 20px",
-               borderRadius: "5px",
-               color: "#fff",
-             }}
+              left:"50%",
+              backgroundColor: "#FF4500",
+              padding: "10px 20px",
+              borderRadius: "5px",
+              color: "#fff",
+            }}
              >
              Voltar ao Topo
            </a>
-        </div>
-      </section>
+       </div>
+        
+     </section>
 
       {/* Footer */}
       <Footer />
